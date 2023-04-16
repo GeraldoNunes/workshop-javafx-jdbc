552c8106fc4ca17cc04faedb61077c908cbedb54
Geraldo Nunes <geraldo.storytelling@gmail.com>
2023-04-16 15:33:59 -0300

    DepartmentList controller

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -44,7 +44,7 @@ public class MainViewController implements Initializable {
 	
 	
 	@Override
-	public synchronized void initialize(URL uri, ResourceBundle rb) {
+	public synchronized void initialize(URL url, ResourceBundle rb) {
 	}
 	
 	private void loadView(String absoluteName) {
